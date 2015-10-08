628f0e25bcd7428cded82a47f59dad4c93435d75
Phil <zoldello@gmail.com>
2015-10-08 15:51:04 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+export class App {
+
+	constructor() {
+		this.message = "People keep ... rearranging ... minds, thoughts, predictions. Somehow we'll work it out";
+	}
+
+	changeMessage() {
+		this.message = "All I wanna say is that they do not really care about us"
+	}
+}
